de7ac98ecaec4bfd0b0a0a03eb5bf5973bc23df8
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2020-09-02 11:03:46 -0300

    Limpeza de código
    Eliminação de código desnecessário de VeiculoController.java

--- cadastra_veiculos/src/main/java/com/lduran/cadastra_veiculos/controller/VeiculoController.java
+++ cadastra_veiculos/src/main/java/com/lduran/cadastra_veiculos/controller/VeiculoController.java
@@ -11,12 +11,12 @@ import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -103,7 +103,7 @@ public class VeiculoController
 		this.veiculoService.excluir(id);
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.PATCH)
+	@PatchMapping(value = "/{id}")
 	public @ResponseBody void saveContact(@PathVariable Long id, @RequestBody Map<Object, Object> fields)
 	{
 		// ResponseEntity<Veiculo> veiculo = this.veiculoService.buscaVeiculo(id);
@@ -115,6 +115,7 @@ public class VeiculoController
 			// use reflection to get field k on manager and set it to value v
 			Field field = ReflectionUtils.findField(Veiculo.class, (String) k);
 
+			// Se o atributo a ser alterado for "vendido", pega o ENUM por meio da posição
 			v = (String) k == "vendido" ? Vendido.values()[Integer.parseInt((String) v)] : v;
 
 			field.setAccessible(true);
